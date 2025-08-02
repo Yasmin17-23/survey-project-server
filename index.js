6fa11facac84afe05a997061b02bc2aba8f0f681
Yasmin17-23 <zyasmins1747@gmail.com>
2025-08-02 22:39:56 +0600

    created api for all survey data from db

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 8000;
-
 
 //middleware
 const corsOptions = {
   origin: [
-     'http://localhost:5173',
-     'http://localhost:5174',
-     'https://survey-project-c091a.web.app'
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://survey-project-c091a.web.app",
   ],
   credentails: true,
   optionSuccessStatus: 200,
-}
+};
 
 app.use(cors(corsOptions));
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hmtao.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -30,7 +27,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -38,30 +35,33 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     //await client.connect();
 
-    const surveyCollection = client.db('surveyBangla').collection('surveys');
+    const surveyCollection = client.db("surveyBangla").collection("surveys");
 
     //Save a survey data in db
-    app.post('/survey', async (req, res) => {
+    app.post("/survey", async (req, res) => {
       const surveyData = req.body;
       surveyData.timestamp = new Date();
       const newSurveyData = {
         ...surveyData,
-        status:  'publish',
+        status: "publish",
         yesCount: 0,
-        noCount : 0
-      }
+        noCount: 0,
+      };
       const result = await surveyCollection.insertOne(newSurveyData);
+      res.send(result);
+    });
+
+    //Get all survey data from db
+    app.get('/surveys', async (req, res) => {
+      const result = await surveyCollection.find().toArray();
       res.send(result)
     })
-    
-    
-
-
-  
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     //await client.close();
@@ -69,12 +69,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send('Survey is going on')
-})
+app.get("/", (req, res) => {
+  res.send("Survey is going on");
+});
 
 app.listen(port, () => {
-    console.log(`Survey Server is running on port: ${port}`)
-    
-})
+  console.log(`Survey Server is running on port: ${port}`);
+});
